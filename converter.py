eebfceceb8b0f56f4cdd389143c9357bd0668d09
Doruk Aytekin <dorukaytekinexxon@gmail.com>
2024-08-19 21:38:46 +0000

    finishing develop

--- converter.py
+++ converter.py
@@ -7,27 +7,32 @@ import pillow_avif
 import pillow_heif
 import os
 import shutil
+import easygui
 
 pillow_heif.register_heif_opener()
 
+inputPath = easygui.diropenbox(title='Select The Input Folder')
+outputPath = easygui.diropenbox(title='Select The Output Folder')
+
+
 sayi = 0
 eklenen = 0
-src = "C:/Users/horat/OneDrive/Masaüstü/Deneme"
-dest = "C:/Users/horat/OneDrive/Masaüstü/Convert"
+#src = "C:/Users/horat/OneDrive/Masaüstü/Deneme"
+#dest = "C:/Users/horat/OneDrive/Masaüstü/Convert"
 convert_to = 'webp'
 to_convert = [".jpg",'png']
-for photo in os.listdir(src):
+for photo in os.listdir(inputPath):
     print(photo)
-for photo in os.listdir(src):
+for photo in os.listdir(inputPath):
    for i in to_convert:
         if i in photo:
             print(photo[photo.index(".")+1:len(photo)])
-            temp_image = Image.open(src + "/" + photo)
+            temp_image = Image.open(inputPath + "/" + photo)
             png_photo = photo.replace(photo[photo.index(".")+1:len(photo)], convert_to)
             print(png_photo)
             temp_image.save(png_photo)
             print(os.path.abspath(png_photo))
-            shutil.move(os.path.abspath(png_photo),dest)
+            shutil.move(os.path.abspath(png_photo),outputPath)
             sayi += 1
             print(f"Donusturulen Dosya Sayisi:{sayi}")
         else:
